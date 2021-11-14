9a78d10d721e8a90909e8041d24b3f9dac00f758
Bruno-Carvalho <brunooakdev@gmail.com>
2021-11-14 12:20:22 -0300

    initial commit

---
+++ src/main/java/com/solilori/nullpaper/NullpaperApplication.java
@@ -0,0 +1,13 @@
+package com.solilori.nullpaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NullpaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NullpaperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solilori/nullpaper/NullpaperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solilori.nullpaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NullpaperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
