435fe189462898821f0564b3fa6cddcdd27d88b8
Bruno-Carvalho <brunooakdev@gmail.com>
2021-11-26 11:32:08 -0300

    H2 security configuration

--- src/main/java/com/solilori/nullpaper/config/ResourceServerConfig.java
+++ src/main/java/com/solilori/nullpaper/config/ResourceServerConfig.java
@@ -2,17 +2,23 @@ package com.solilori.nullpaper.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-    private static final String[] PUBLIC = {"/oauth/token"};
+    @Autowired
+    private Environment environment;
+
+    private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
 
     private static final String[] OPERATOR_OR_ADMIN = {"/customers/**", "/printers/**", "readings/**"};
 
@@ -28,6 +34,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        // Unlock h2-console
+        if (Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll()
                 .antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
